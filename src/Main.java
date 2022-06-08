09d5e47927b224ee0bec1fcc089ad2c9bcf47fdf
NabokaEA <nabokae@yandex.ru>
2022-06-08 19:13:28 +0300

    6. Добавлены преобразования операторов в числовые значения

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Byte.parseByte;
+
 public class Main {
     static String result; //результат
     static String inputString; // строка ввода пользовательского выражения
@@ -20,22 +22,36 @@ public class Main {
     //Метод принимает строку с арифметическим выражением между двумя числами и возвращет строку с результатом их выполнения.
     public static String calc(String input) {
         //Получим массив строковых операторов:
-        String[] opArray=inputStringToArray(input);
+        String[] opArrayStrings = inputStringToArray(input);
+        byte[] opArrayBytes = new byte[2];
         //Проверка количества операций на неравенство 2:
-        if (opArray.length!=2){
+        if (opArrayStrings.length != 2) {
             throw new RuntimeException("Математическая операция не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
         //Проверим операнды на принадлежность к римской или арабской системе:
-        if (isRoman(opArray[0])&isRoman(opArray[1])){
+        if (isRoman(opArrayStrings[0]) & isRoman(opArrayStrings[1])) {
             System.out.println("Римские");
-        } else if (!isRoman(opArray[0])&!isRoman(opArray[1])) {
+            //Римские переобразуем в массив байт для вычислений с помощью метода
+            for (int i = 0; i < 2; i++) {
+                opArrayBytes[i] = romanStringToArabByts(opArrayStrings[i]);
+                System.out.println(opArrayBytes[i]);
+            }
+        } else if (!isRoman(opArrayStrings[0]) & !isRoman(opArrayStrings[1])) {
             System.out.println("Арабские");
-        } else {throw new RuntimeException("используются одновременно разные системы счисления");}
+            //Арабские переобразуем в массив байт для вычислений парсингом
+            for (int i = 0; i < 2; i++) {
+                opArrayBytes[i] = arabStringToArabByts(opArrayStrings[i]);
+                System.out.println(opArrayBytes[i]);
+            }
+        } else {
+            throw new RuntimeException("используются одновременно разные системы счисления");
+        }
 
 
         return "ОК";
     }
-// Метод преобразует строку в строковый массив, состоящий из операторов
+
+    // Метод преобразует строку в строковый массив, состоящий из операторов
     private static String[] inputStringToArray(String input2) {
         if (inputString.contains("+")) {
             String[] inputAsArray = inputString.split("[+]");
@@ -53,17 +69,31 @@ public class Main {
             String[] inputAsArray = inputString.split("[*]");
             Operation operation = Operation.MUL;
             return inputAsArray;
-        } else{
+        } else {
             throw new RuntimeException("Строка не является математической операцией");
         }
     }
+
     // Метод определения системы, к которой относится сначение операнда
-    private static boolean isRoman (String operator){
-            for (RomanDigits digit : RomanDigits.values()) {
-                if (digit.name().equals(operator)) {
-                    return true;
-                }
+    private static boolean isRoman(String operator) {
+        for (RomanDigits digit : RomanDigits.values()) {
+            if (digit.name().equals(operator)) {
+                return true;
             }
-            return false;
         }
+        return false;
+    }
+
+    private static byte romanStringToArabByts(String romanDigit) {
+        for (RomanDigits digit : RomanDigits.values()) {
+            if (digit.name().equals(romanDigit)) {
+                return (byte) digit.ordinal();
+            }
+        }
+        return 127;
+    }
+
+    private static byte arabStringToArabByts(String arabDigit) {
+        return parseByte(arabDigit);
+    }
 }
