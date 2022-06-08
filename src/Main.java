e014c00110c5046d86a41f722b396c4ddaea2257
NabokaEA <nabokae@yandex.ru>
2022-06-08 13:47:16 +0300

    2. добавлена сигнатура метода calc

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+    static String result; //результат
+    static String inputString; // строка ввода пользовательского выражения
+    //Блок инициализации переменных result=a+-*/b
+    byte a = 0; //первый операнд
+    byte b = 0; //второй операнд
+
     public static void main(String[] args) {
-//Блок инициализации переменных result=a+-*/b
-        byte a = 0; //первый операнд
-        byte b = 0; //второй операнд
-        byte result = 0; //результат
-        String inputString; // строка ввода пользовательского выражения
+
+        // Читаем с консоли введееные данные
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите арифметическое выражение типа a+(-*/)b=с арабскими, либо с римскими числами до 10:");
-        inputString =scanner.next();
+        inputString = scanner.next();
         System.out.printf("Введена строка: %s ", inputString);
     }
 
+    //Метод принимает строку с арифметическим выражением между двумя числами и возвращет строку с результатом их выполнения.
+    public static String calc(String input) {
+
+
+        return result;
+    }
 }
